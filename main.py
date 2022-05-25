b57739a9df231ec4e696ba32c81638e9bf65e137
Diana <qwe@qwe.qwe>
2022-05-25 09:52:20 +0300

    fix

---
+++ main.py
@@ -0,0 +1,23 @@
+# print("Result:", 5 // 3)
+# print("Result:", min (5, 3, 7, 2, 0, -3))
+# print("Result:", abs(-5))
+# print("Result: ", 7, 15, sep="", end="!\n")
+# print("Second line")
+
+# input("Enter yout age: ")
+
+number = 5 # int
+
+digit = 4.554455 # float
+word = "Result: " # string
+boolean = True # bool
+
+str_num = "5" # string
+
+# print(word + str(digit))
+
+print(word + str(number + int(str_num)))
+
+number = 7
+print("Result:", number)
+
---
+++ test.py
@@ -0,0 +1,15 @@
+num1 = int(input("Enter first number: "))
+
+num2 = int(input("Enter second number: "))
+
+num1 += 5
+
+print("Result: ", num1 + num2)
+print("Result: ", num1 - num2)
+print("Result: ", num1 / num2)
+print("Result: ", num1 * num2)
+print("Result: ", num1 ** num2)
+print("Result: ", num1 // num2)
+
+word = "Hi " 
+print(word * 5)
